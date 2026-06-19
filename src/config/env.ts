df4d4abc61866de0b593bde05e0816500dc458c2
yogie <lordz0990@gmail.com>
2026-06-19 23:28:48 +0700

    .

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,11 @@ export const env = {
   supabaseAnonKey: import.meta.env.VITE_SUPABASE_ANON_KEY as string | undefined,
 }
 
+console.log("===== ENV DEBUG =====");
+console.log("URL :", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY :", import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log(import.meta.env);
+
 export function assertEnv() {
   if (!env.supabaseUrl || !env.supabaseAnonKey) {
     throw new Error('Missing Supabase environment variables.')
